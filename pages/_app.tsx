dc801a004df4ef237b3b62d95c8cb04c480b5e4f
Hamza <untilhamza@gmail.com>
2022-08-24 02:37:37 +0900

    use react-spectrum layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
-import "../styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
-import MainNav from "../components/MainNav";
-import Layout from "../components/Layout";
+import { defaultTheme, Provider, SSRProvider } from "@adobe/react-spectrum";
+import MainLayout from "../components/MainLayout";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <MainNav />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <SSRProvider>
+      <Provider theme={defaultTheme}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </Provider>
+    </SSRProvider>
   );
 }
 
